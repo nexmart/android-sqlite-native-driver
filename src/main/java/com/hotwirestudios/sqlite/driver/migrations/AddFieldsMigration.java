a44260e0db9ed126c6b72481d5e4936e70ef92db
Fabian Muecke <fabian@hotwirestudios.com>
2016-12-19 16:47:52 +0100

    Added a default migration to add fields to an existing table

---
+++ src/main/java/com/hotwirestudios/sqlite/driver/migrations/AddFieldsMigration.java
@@ -0,0 +1,43 @@
+package com.hotwirestudios.sqlite.driver.migrations;
+
+import com.hotwirestudios.sqlite.driver.SQLiteConnection;
+
+/**
+ * Created by FabianM on 19.12.16.
+ */
+
+public class AddFieldsMigration extends Migration {
+    private final String name;
+    private final String table;
+    private final Field[] fields;
+
+    public AddFieldsMigration(long migrationId, String name, String table, Field[] fields) {
+        super(migrationId);
+        this.name = name;
+        this.table = table;
+        this.fields = fields;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    protected void executeImpl(SQLiteConnection connection) throws Exception {
+        for (Field field : fields) {
+            String sql = "ALTER TABLE \"" + table + "\" ADD COLUMN \"" + field.name + "\" " + field.type;
+            connection.executeStatement(sql);
+        }
+    }
+
+    public static class Field {
+        private String name;
+        private String type;
+
+        public Field(String name, String type) {
+            this.name = name;
+            this.type = type;
+        }
+    }
+}
